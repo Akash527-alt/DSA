6fe8dc26872bf11d7cdf651e8be6076f83b953e1
akash <kumarakash386534@gmial.com>
2025-08-15 20:32:41 +0530

    added new sorting methods

--- Sorting1/RunSorting.java
+++ Sorting1/RunSorting.java
@@ -5,11 +5,16 @@ import java.util.Arrays;
 class RunSorting{
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] nums = {8,2,5,2,1,6,7,3};
-         System.out.println("Unsorted array: "+Arrays.toString(nums));
-        // System.out.println("Sorted array using selection sort: "+Arrays.toString(s.selectionSort(nums)));
+    //     int[] nums = {8,2,5,2,1,6,7,3};
+    //      System.out.println("Unsorted array: "+Arrays.toString(nums));
+    //    // System.out.println("Sorted array using selection sort: "+Arrays.toString(s.selectionSort(nums)));
 
-        System.out.println("Bubble sort: "+Arrays.toString(s.bubbleSort(nums)));
+        // System.out.println("Bubble sort: "+Arrays.toString(s.bubbleSort(nums)));
+
+        int[] arr1 = {2,3,5,7,1,2,8,9,10,4};
+        System.out.println("\nunsorted Array: "+Arrays.toString(arr1));
+       // System.out.println("Insertion sort: "+Arrays.toString(s.insertionSort(arr1)));
+        System.out.println("Insertion sort 2: "+Arrays.toString(s.insertionsSort2(arr1)));
 
     }
 
@@ -20,15 +25,44 @@ class RunSorting{
 
 class Solution {
 
-    public int[] bubbleSort(int[] nums) {
+    public int[] insertionsSort2(int[] arr){
+        int n=arr.length;
+        for(int i=1;i<n;i++){
+            int key = arr[i];
+            int j= i-1;
+            while(j>=0 && arr[j]>key){
+                arr[j+1] = arr[j];
+                j--;
+                System.out.println("  Current array: "+Arrays.toString(arr));
+            }
+            arr[j+1] = key;
+        }
+        return arr;
+    }
+
+
+     public int[] insertionSort(int[] nums) {
         for(int i=0;i<nums.length;i++){
+            int j = i;
+            while(j>0 && nums[j-1] >nums[j]){
+                int temp = nums[j];
+                nums[j] = nums[j-1];
+                nums[j-1] = temp;
+                j--;
+            }
+        }
+        return nums;
+    }
+
+    public int[] bubbleSort(int[] nums) {
+        for(int i=0;i<nums.length-1;i++){
             for(int j=0;j<nums.length-i-1;j++){
                 if(nums[j]>nums[j+1]){
                     int temp = nums[j];
                     nums[j] = nums[j+1];
                     nums[j+1] = temp;
                 }
-                System.out.println(Arrays.toString(nums));
+                // System.out.println(Arrays.toString(nums));
             }
         }
         return nums;
@@ -37,7 +71,7 @@ class Solution {
 
     public int[] selectionSort(int[] nums) {
         for(int i=0;i<nums.length-1;i++){
-            int minInd = nums[i];
+            int minInd = i;
             for(int j=i+1;j<nums.length;j++){
                 if(nums[minInd] > nums[j]){
                     minInd = j;
@@ -53,6 +87,8 @@ class Solution {
         return nums;
 
     }
+
+
 
    
     }
