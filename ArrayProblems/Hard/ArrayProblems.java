13c99950205b31182aaabf8f46b7b3f205c8ac09
akash <kumarakash386534@gmial.com>
2025-10-25 18:57:30 +0530

    solved more arrays problems

--- ArrayProblems/Hard/ArrayProblems.java
+++ ArrayProblems/Hard/ArrayProblems.java
@@ -5,17 +5,16 @@ import java.util.*;
 public class ArrayProblems {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] nums = {1,0,-1,0,-2,2};
-        int target = 0;
 
-        List<List<Integer> > ans = s.fourSum(nums,target);
 
-        System.out.println(ans);
-//        for(List<Integer> a: ans){
-//            for(int ele: a){
-//
-//            }
-//        }
+        int arr2[] = {4,3,6 ,2, 1, 1};
+
+        int[] ans = s.findMissingRepeatingNumbers(arr2);
+
+        for(int a: ans){
+            System.out.print(a + " ");
+        }
+
 
 
     }
@@ -23,6 +22,219 @@ public class ArrayProblems {
 
 class Solution {
 
+
+    public int[] findMissingRepeatingNumbers(int[] nums) {
+
+
+        // solution 3 : optimal 1 -> Mathematics calculation  -> tc - O(n)
+
+        // s - sn : x-y
+        // s2 - sn2 : x^2 - y^2
+
+        int n = nums.length;
+        long sn = ((long) n * (n+1))/2;
+        long sn2 = (n * (n+1) * (2 * n + 1)) /6;
+        long s = 0;
+        long s2 = 0 ;
+        for(int i = 0 ; i<n; i++){    // O(n)
+            s += nums[i];
+            s2 += (long) nums[i] * (long)nums[i];
+        }
+
+        long val1 = s - sn;  // x - y
+        long val2 = s2 - sn2;  // x^2 - y^2
+        val2 = val2/val1;
+
+        long x = (val1 + val2)/2;
+        long y = val2- x;
+
+        return new int[]{(int)x, (int)y};
+
+
+        // solution 2 : Better -> using hashMap
+        /*
+        int n = nums.length;
+        int[] hash = new int[n+1];
+
+        for(int i=0;i<n;i++){
+            hash[nums[i]]++;
+        }
+
+        int repeating = -1, missing =-1;
+        for(int i = 1;i<=n ;i++){
+            if(hash[i] == 2) repeating = i;
+            else if(hash[i] == 0) missing = i;
+
+            if(repeating != -1 && missing != -1){
+                break;
+            }
+        }
+
+        int ans[] = {repeating,missing};
+        return ans;
+
+         */
+
+
+        // brute force solution
+        /*
+        int n = nums.length;
+        int missing = -1;
+        int repeating = -1;
+
+        for(int i =1 ; i<=n; i++){
+            int count = 0;
+            for(int j = 0 ;j<n;j++){
+                if(nums[j] == i) count ++;
+            }
+
+            if(count == 0) missing = i;
+            else if(count==2) repeating = i;
+
+            if(missing != -1 && repeating != -1) break;
+
+
+
+        }
+
+        int[] ans = {repeating,missing};
+        return ans;
+
+         */
+
+
+    }
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        // solution 2
+        int i = m-1;
+        int j = n-1;
+        int k = m+n -1;
+
+        while(j>=0 && i>= 0){
+            if(nums1[i]>nums2[j]){
+                nums1[k--] = nums1[i--];
+            }
+            else{
+                nums1[k--] = nums2[j--];
+            }
+        }
+
+        while(j >= 0 ){
+            nums1[k--] = nums2[j--];
+        }
+
+
+
+        /**
+         *
+
+        // method 1
+        for(int i = 0 ; i<n; i++){
+            nums1[i+m] = nums2[i];
+        }
+
+        Arrays.sort(nums1);
+
+         */
+
+
+    }
+
+    public List<List<Integer>> mergeOverlap(List<List<Integer>> intervals) {
+
+
+
+        intervals.sort((a,b) -> {
+           if(!a.get(0).equals(b.get(0))){
+               return Integer.compare(a.get(0),b.get(0));
+           }
+           else{
+               return Integer.compare(a.get(1),b.get(1));
+           }
+        });
+
+        List<List<Integer>> ans = new ArrayList<>();
+//        // method 2
+//
+//        for(List<Integer> interval: intervals){
+//            if(ans.isEmpty() || ans.getLast().get(1)<interval.get(0)){
+//                ans.add(Arrays.asList(interval.getFirst(),interval.getLast()));
+//            }
+//            else{
+//                List<Integer> last = ans.getLast();
+//                int maxEnd = Math.max(ans.get(last.getFirst()).get(1),interval.get(1));
+//
+//            }
+//
+//        }
+
+
+      //   method 1
+        int n = intervals.size();
+        int i=0 ;
+        while(i<n){
+            int start = intervals.get(i).get(0);
+            int end = intervals.get(i).get(1);
+
+            int j =i+1;
+
+            while(j<n && intervals.get(j).getFirst() <= end){
+                end = Math.max(end,intervals.get(j).getLast());
+                j++;
+
+            }
+
+            ans.add(Arrays.asList(start,end));
+            i = j;
+
+        }
+
+        return ans;
+    }
+
+    public int[][] merge(int[][] intervals) {
+
+        Arrays.sort(intervals, (a,b) -> {
+            if(a[0] != b[0]){
+                return Integer.compare(a[0],b[0]);
+            }
+            else{
+                return Integer.compare(a[1],b[1]);
+            }
+        });
+
+        int n = intervals.length;
+        List<int[]> merged = new ArrayList<>();
+
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+
+        for(int i = 0;i < n;i++){
+
+            int[] current  = intervals[i];
+
+            if(current[0]<=end){
+                end = Math.max(end,current[1]);
+            }
+            else{
+                merged.add(new int[]{start,end});
+                start = current[0];
+                end = current[1];
+            }
+
+
+
+        }
+
+            merged.add(new int[]{start,end});
+
+            return merged.toArray(new int[merged.size()][]);
+
+
+
+    }
 
     public List<List<Integer>> fourSum(int[] nums, int target) {
         List<List<Integer>> ans  = new ArrayList<>();
